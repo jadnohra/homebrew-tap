f330e042c159767d643df989fcd0039594846c93
Jad Nohra <jadnohra@gmail.com>
2026-02-07 19:05:10 +0100

    Update recent-work formula with v1.0.0 SHA256

--- Formula/recent-work.rb
+++ Formula/recent-work.rb
@@ -2,7 +2,7 @@ class RecentWork < Formula
   desc "Watches your projects, symlinks recent files to ~/RecentWork"
   homepage "https://github.com/jadnohra/recent-work"
   url "https://github.com/jadnohra/recent-work/releases/download/v1.0.0/recent-work-macos-universal.tar.gz"
-  sha256 "UPDATE_AFTER_FIRST_RELEASE"
+  sha256 "b0172c270407a913e4b671786488ecc8e14a6d97b449f9372db43a50b9f4e46e"
   license "MIT"
 
   def install
